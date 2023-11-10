d3d125e04f6afb2610e6c432c8a0d26cd28a129e
khushi <139205399+Ambkhushi@users.noreply.github.com>
2023-11-10 16:11:38 +0530

    Create Treeset.java

---
+++ Treeset.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+                               TreeSet
+*******************************************************************************/
+import java.util.*;    
+class Tree{    
+ public static void main(String args[]){    
+ TreeSet<Integer> set=new TreeSet<Integer>();    
+         set.add(24);    
+         set.add(66);    
+         set.add(12);    
+         set.add(15);    
+          Iterator i=set.descendingIterator();  
+         while(i.hasNext())  
+         {  
+             System.out.println(i.next());  
+         }  
+             System.out.println("Lowest Value: "+set.pollFirst());    
+         System.out.println("Highest Value: "+set.pollLast());  
+          System.out.println("Initial Set: "+set); 
+ }  
+}  
